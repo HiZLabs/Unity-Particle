7d8d50a5e1ed83d9ef958d3fcae7e3c89acc2ad9
Aaron Heise <aaron.heise@gmail.com>
2016-08-27 16:06:44 -0500

    Stuff for writing tests

--- firmware/UnityHelper.cpp
+++ firmware/UnityHelper.cpp
@@ -5,6 +5,7 @@
  *      Author: aaron
  */
 #include "application.h"
+#include <list>
 
 static Print* print = &Serial;
 
@@ -18,5 +19,43 @@ void __unity_set_print(Print& output) {
 
 Print* __unity_get_print() {
 	return print;
+}
+
+struct test {
+	const char* name;
+	void (*func)();
+};
+
+struct test_group {
+	const char* name;
+	std::list<test> tests;
+};
+
+static std::list<test_group> groups;
+
+static std::list<test_group>::iterator find_group(const char* group_id) {
+	std::list<test_group>::iterator i = groups.begin();
+	while (i != groups.end()) {
+		if (strcmp(group_id, i->name) == 0)
+			break;
+		i++;
+	}
+	return i;
+}
+
+void __unity_register_test(const char* group_id, const char* test_id,
+		void (*func)()) {
+	std::list<test_group>::iterator group = find_group(group_id);
+	if (group == groups.end()) {
+		test_group newGroup = { group_id };
+		groups.push_back(newGroup);
+		group = find_group(group_id);
+	}
+
+
+}
+
+int __unity_run_tests() {
+
 }
 
--- firmware/unity_config.h
+++ firmware/unity_config.h
@@ -19,14 +19,29 @@ void __unity_print_char(int c);
 #ifdef __cplusplus
 } // extern "C" { //from unity_internals.h
 
+#include <list>
 #include "application.h"
 void __unity_set_print(Print& output);
 Print* __unity_get_print();
+
+void __unity_register_test(const char* group_id, const char* test_id,
+		void (*func)());
+
+void __unity_run_tests();
+
+
 #define UNITY_SET_PRINT(print) __unity_set_print(print)
 #define UNITY_PRINT(...) __unity_get_print()->print(__VA_ARGS__)
 #define UNITY_PRINTF(...) __unity_get_print()->printf(__VA_ARGS__)
 #define UNITY_PRINTLN(...) __unity_get_print()->println(__VA_ARGS__)
 #define UNITY_PRINTLNF(...) __unity_get_print()->printlnf(__VA_ARGS__)
+
+#ifdef TEST
+#undef TEST
+#endif
+
+#define TEST(group_id, test_id) void test_id(); __unity_register_test(#group_id, #test_id, test_id); void test_id()
+#define UNITY_RUN_TESTS() __unity_run_tests()
 
 extern "C" {
 #endif
